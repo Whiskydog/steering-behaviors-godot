cdf98ef4603ea682a2e2342871d9bf03825dcbe2
Whiskydog <alan.varde@gmail.com>
2019-11-18 08:09:27 -0300

    Implemented separate behavior

---
+++ scripts/tests/test_separation.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+onready var width = get_viewport().size.x
+onready var height = get_viewport().size.y
+var boids = []
+
+func _ready():
+	for i in range(100):
+		var boid = preload('res://scenes/steerable.tscn').instance()
+		add_child(boid, true)
+		boid.position.x = randf() * width
+		boid.position.y = randf() * height
+		boid.add_behavior($Path, 'follow_path')
+		boid.add_behavior(boids, 'separate')
+		boids.append(boid)
